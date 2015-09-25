adb2f0f011ab2433af0b558739e172f08bb32926
Chester Ismay <chester.ismay@gmail.com>
2015-09-25 12:40:58 -0700

    Just started on Data Vis.  More to come!

--- Data_Vis_using_R_workshop_2015/script.R
+++ Data_Vis_using_R_workshop_2015/script.R
@@ -6,18 +6,8 @@ library(DCF)
 library(DCFinteractive)
 DCFinteractive::mScatter(CountryData)
 
-
-
 # Use DCFinteractive for data visualization, look into Networks pages 99-101
 
 # Focus on not hard coding, frequently compiling, and saving
 
-# Page 39 for Rmd
-
-
-"http://reed.edu/data-at-reed/software/R/workshops/2015/weather.RDS"
-"http://reed.edu/data-at-reed/software/R/workshops/2015/flights.RDS"
-
-# This is the case with e.g. qplot, but note the data = . argument. 
-# This tells %>% to place the left-hand side there, and not as the first argument. 
-babynames %>% filter(name == "Chester") %>% qplot(x = year, y = n, data = .)
+# Page 39 of Data Computing for Rmd
