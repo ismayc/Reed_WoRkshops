86312ba5f099860f6fd3a40c08ce51eee841058d
ismayc <chester.ismay@gmail.com>
2016-01-11 12:03:44 -0800

    Updated with links

---
+++ january_academy_2016/slides/rpubs.R
@@ -0,0 +1,11 @@
+if(!require(markdown))
+  install.packages("markdown", repos = "https://cran.rstudio.com")
+library(markdown)
+
+#result <- markdown::rpubsUpload(title = "Data Viz - January Academy 2016", 
+#  htmlFile = "slides/slides.html")
+result2 <- markdown::rpubsUpload(title = "Data Viz - January Academy 2016", 
+  htmlFile = "slides/slides.html",
+  id = result$id)
+if (!is.null(result2$continueUrl)) 
+  browseURL(result2$continueUrl) else stop(result2$error)
