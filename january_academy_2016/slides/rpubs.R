8d69efebb5775f5af15d5de0e5ed4beefdad4e2c
ismayc <chester.ismay@gmail.com>
2016-01-13 13:33:23 -0800

    Added inference examples

--- january_academy_2016/slides/rpubs.R
+++ january_academy_2016/slides/rpubs.R
@@ -3,9 +3,9 @@ if(!require(markdown))
 library(markdown)
 
 #result <- markdown::rpubsUpload(title = "Data Viz - January Academy 2016", 
-#  htmlFile = "slides/slides.html")
+# htmlFile = "january_academy_2016/slides/slides.html")
 result2 <- markdown::rpubsUpload(title = "Data Viz - January Academy 2016", 
-  htmlFile = "slides/slides.html",
+  htmlFile = "january_academy_2016/slides/slides.html",
   id = result$id)
 if (!is.null(result2$continueUrl)) 
   browseURL(result2$continueUrl) else stop(result2$error)
